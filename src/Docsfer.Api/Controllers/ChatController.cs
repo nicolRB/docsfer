12596eb61cc093393779907a67af8e309ada9639
nicolRB <134354240+nicolRB@users.noreply.github.com>
2025-09-03 22:25:02 -0300

    Add files via upload

--- src/Docsfer.Api/Controllers/ChatController.cs
+++ src/Docsfer.Api/Controllers/ChatController.cs
@@ -1,4 +1,8 @@
+﻿using Docsfer.Api.Repositories;
+using Docsfer.Core.Chat;
+using Docsfer.Core.Identity;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Docsfer.Api.Controllers;
@@ -8,4 +12,48 @@ namespace Docsfer.Api.Controllers;
 [Route("api/v1/chat")]
 public class ChatController : ControllerBase
 {
+    private readonly IChatRepository _chatRepository;
+    private readonly IBlobEntryRepository _blobEntryRepository;
+    private readonly UserManager<User> _userManager;
+
+    public ChatController(
+        IChatRepository chatRepository, 
+        IBlobEntryRepository blobEntryRepository, 
+        UserManager<User> userManager)
+    {
+        _chatRepository = chatRepository;
+        _blobEntryRepository = blobEntryRepository;
+        _userManager = userManager;
+    }
+
+    [HttpPost("{blobId}/message")]
+    public async Task<IActionResult> PostMessage(long blobId, PostMessageInput input)
+    {
+        var user = await _userManager.GetUserAsync(User);
+
+        var blob = await _blobEntryRepository.FindByIdAsync(blobId);
+        if (blob == null)
+        {
+            return NotFound("Arquivo não encontrado");
+        }
+
+        var message = new ChatMessage
+        {
+            BlobEntryId = blob.Id.Value,
+            UserId = user.Id,
+            Content = input.Content,
+            CreatedAt = DateTime.UtcNow
+        };
+
+        await _chatRepository.InsertAsync(message);
+
+        return Ok(message);
+    }
+
+    [HttpGet("{blobId}/messages")]
+    public async Task<IActionResult> GetMessages(long blobId)
+    {
+        var messages = await _chatRepository.GetByBlobAsync(blobId);
+        return Ok(messages);
+    }
 }
