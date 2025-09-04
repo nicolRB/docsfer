f565558e3f1a1dac0bbc1894898b8d47c0275c1d
Joao-AZ <jpmizidoro@gmail.com>
2025-09-04 20:54:10 +0000

    starting aside component

--- src/Docsfer.Web/src/components/PageAside.tsx
+++ src/Docsfer.Web/src/components/PageAside.tsx
@@ -1,9 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
+import { House, ChevronsRight, Home } from "lucide-react";
 
 const PageAside = () => {
-  return (
-    <nav className="absolute h-[calc(100dvh-48px)] w-12 dark:bg-zinc-800"></nav>
-  );
+  const [isExpanded, setIsExpanded] = useState(false);
+  const [isPinned, setIsPinned] = useState(false);
+
+  const menuItems = [{ icon: Home, label: "Dashboard" }];
+
+  const handleMouseEnter = () => {
+    if (!isPinned) {
+      setIsExpanded(true);
+    }
+  };
+
+  const handleMouseLeave = () => {
+    if (!isPinned) {
+      setIsExpanded(false);
+    }
+  };
+
+  const togglePin = () => {
+    setIsPinned(!isPinned);
+    setIsExpanded(!isExpanded);
+  };
+
+  return <div className="relative group hidden lg:block text-zinc-700"></div>;
 };
 
 export default PageAside;
