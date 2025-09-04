3e9b5b1b8a7f8eff801aaf9ca1276518e6daf7a3
nicolRB <134354240+nicolRB@users.noreply.github.com>
2025-09-03 22:28:03 -0300

    Add files via upload

---
+++ src/Docsfer.Api/Repositories/ChatRepository.cs
@@ -0,0 +1,19 @@
+﻿using Docsfer.Core.Chat;
+
+namespace Docsfer.Api.Repositories;
+
+public class ChatRepository : IChatRepository
+{
+    private readonly List<ChatMessage> _messages = new();
+
+    public Task InsertAsync(ChatMessage message)
+    {
+        _messages.Add(message);
+        return Task.CompletedTask;
+    }
+    public Task<IEnumerable<ChatMessage>> GetByBlobAsync(long blobEntryId)
+    {
+        var result = _messages.Where(m => m.BlobEntryId == blobEntryId);
+        return Task.FromResult(result.AsEnumerable());
+    }
+}
